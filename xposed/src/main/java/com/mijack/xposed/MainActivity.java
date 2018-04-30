0715a03ed60b70444942f23076ed4ef438fc2082
Mi&Jack <mijackstudio@gmail.com>
2018-04-30 16:11:13 +0800

    add app

--- xposed/src/main/java/com/mijack/xposed/MainActivity.java
+++ xposed/src/main/java/com/mijack/xposed/MainActivity.java
@@ -6,14 +6,11 @@ import android.os.Bundle;
 
 public class MainActivity extends Activity /*implements View.OnClickListener*/ {
 
-	private static final int REQUEST_CODE = 1;
-
-
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 //		setContentView(R.layout.activity_main);
 //		findViewById(R.id.btn).setOnClickListener(this);
-	}
+    }
 
 }
